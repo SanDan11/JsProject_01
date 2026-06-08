b54a50994243062a2cb6477d260e6643dd19c8b4
Daniel <Danielrsanchez1990@gmail.com>
2026-06-08 09:33:01 -0700

    Project Created and HTML and JS working properly

---
+++ app.js
@@ -0,0 +1,20 @@
+
+const title = document.getElementById("title");
+const message = document.getElementById("message");
+const search = document.getElementById("search");
+const reset = document.getElementById("reset");
+const searchBar = document.getElementById("searchBar");
+const nameDisplay = document.getElementById("nameDisplay");
+
+
+search.addEventListener("click", function () {
+    alert("Cd is being searched!");
+});
+
+reset.addEventListener("click", function () {
+    alert("Search reset");
+});
+
+searchBar.addEventListener("input", function() {
+
+});
